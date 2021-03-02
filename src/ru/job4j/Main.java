5a7cde2b163a412977106f92d7ef81d1b924e7cc
000SergeyMayer000 <samajor@mail.ru>
2021-03-02 15:02:35 +0300

    6.8. Дефрагментация массива. [#235447]

--- src/ru/job4j/Main.java
+++ src/ru/job4j/Main.java
@@ -1,10 +1,10 @@
 package ru.job4j;
 
 public class Main {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("внес изменения");
-        String[] str = new String [] {"Sergej", "Konstantin", "Oleg"};
+        String[] str = new String[] {"Sergej", "Konstantin", "Oleg"};
         for (String s : str) {
             System.out.println(s);
         }
---
+++ src/ru/job4j/condition/Point.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition;
+
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
+
+public class Point {
+
+    /**
+     * Это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int x;
+
+    /**
+     * И это поле объекта. Оно доступно только конкретному объекту.
+     */
+    private int y;
+
+    public double distance(int x1, int y1, int x2, int y2) {
+        return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+    }
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public static void main(String[] args) {
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
+    }
+}
